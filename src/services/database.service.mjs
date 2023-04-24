63e712c4ffa390dbe2a5a172cb87e49ad83c05a1
Surajpandey7 <124279555+Surajpandey7@users.noreply.github.com>
2023-04-24 18:14:52 +0545

    adding service for top n country and city by id

--- src/services/database.service.mjs
+++ src/services/database.service.mjs
@@ -1,4 +1,6 @@
 import mysql from "mysql2/promise";
+import City from "../models/city.mjs";
+import Country from "../models/country.mjs";
 
 
 export default class DatabaseService {
@@ -16,10 +18,14 @@ export default class DatabaseService {
             database: "world",
         });
 
+        if (conn) {
+            console.log('Connected');
+        }
+
         return new DatabaseService(conn);
     }
 
-    async getAllCities() {
+    getAllCities = async () => {
         try {
             const query = `SELECT * FROM City;`;
             const cities = await this.conn.execute(query);
@@ -29,15 +35,53 @@ export default class DatabaseService {
             return [];
         }
     }
-    
-    async getTopPopulatedCities() {
+
+
+    getNPopulatedCountries = async (N) => {
         try {
-            const query = " SELECT Name AS City, Population FROM city ORDER BY Population DESC LIMIT 10;";
+            const query = `SELECT Code, Name, Continent, Region, Population, Capital
+                                FROM country
+                                ORDER BY Population DESC
+                                LIMIT ${N};`;
             const cities = await this.conn.execute(query);
             return cities;
-        }catch(error) {
-            console.error(error);
+        } catch (error) {
+            console.error(err);
             return [];
         }
     }
+
+
+  async getCityById(cityId) {
+    const sql = `
+        SELECT city.*, country.Name AS Country, country.Region, country.Continent, country.Population as CountryPopulation
+        FROM city
+        INNER JOIN country ON country.Code = city.CountryCode
+        WHERE city.ID = ${cityId}
+    `;
+    const [rows, fields] = await this.conn.execute(sql);
+   
+    const data = rows[0];
+
+    const country = new Country(
+      data.Code,
+      data.Country,
+      data.Continent,
+      data.Region,
+      data.CountryPopulation
+    );
+
+    const city = new City(
+        data.ID,
+        data.Name,
+        data.CountryCode,
+        data.District,
+        data.Population
+      );
+
+    city.country = country;
+    return city;
+  }
+
+
 }
